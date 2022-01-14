5de7c1f194ae84249a43c8800a47f14f75d12a0c
isanaid <eizisana@gmail.com>
2022-01-14 07:03:20 +0700

    loading

--- asynch/callback.js
+++ asynch/callback.js
@@ -2,8 +2,10 @@ const xhr = new XMLHttpRequest();
 
 
 const getData = (url, cb)=> {
+    document.getElementById('list').innerHTML = `<tr> <th scope="row"></th> <td></td> <td></td> <td>Loading...</td> <td></td> <td></td> </tr>`;
     xhr.onload = function () {
         if (xhr.status === 200) {
+            document.getElementById('list').innerHTML = '';
             return cb(JSON.parse(xhr.responseText));
         }
     };
